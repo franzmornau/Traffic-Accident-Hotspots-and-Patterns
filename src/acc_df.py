b91672103d55035863d86853588da2f728c49644
Franz Mornau <franzmornau@Franzs-MBP.home>
2019-07-02 22:33:55 -0700

    time series

--- src/acc_df.py
+++ src/acc_df.py
@@ -1,9 +1,37 @@
-    class Acc_data:
+import pandas as pd
+
+class Acc_data:
     ''' 
-    *STILL A STUBB, UNCLEAR IF NEEDED AT ALL*
     - read accident data from the Seattle source
     - further cleaning
     - provide slices from it
+    (to be further enhanced, with bicyle accident data, pedestrians, etc.)
     '''
-    def __init__(self,data_path):   
-        self.original_acc_data = pd.read_csv(data_path, low_memory=False
+    def __init__(self, file_path):   
+        self._original_acc_data = pd.read_csv(file_path, low_memory=False)
+
+    def get_all_coll(self):
+        '''
+        get the full dataframe
+        Paramteters: none
+        returns: Pandas DF
+        '''
+        return self._original_acc_data.copy()
+    
+    def get_pure_mv_coll(self):
+        '''
+        delivers DF with only the collisions between motor vehicles
+        Paramteters: none
+        returns: Pandas DF
+        '''
+        pmvc =['MOTOR VEHICLE STRUCK MOTOR VEHICLE, FRONT END AT ANGLE',
+                'MOTOR VEHICLE STRUCK MOTOR VEHICLE, REAR END',
+                'MOTOR VEHICLE STRUCK MOTOR VEHICLE, LEFT SIDE SIDESWIPE',
+                'MOTOR VEHICLE STRUCK MOTOR VEHICLE, LEFT SIDE AT ANGLE',
+                'MOTOR VEHICLE STRUCK MOTOR VEHICLE, RIGHT SIDE SIDESWIPE',
+                'MOTOR VEHICLE STRUCK MOTOR VEHICLE, RIGHT SIDE AT ANGLE']
+        
+        X = self._original_acc_data
+        X_pmvc = X[X['SDOT_COLDESC'].isin(pmvc)].copy()
+
+        return X_pmvc
